dd03866890ccd9e0442e721f20328d628a987916
Mauricio Acevedo <mauricio.acevedo@tigoune.com.co>
2018-08-01 08:31:09 -0500

    First commit!
    First commit!

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Page1 from './components/Page1';
+
+class App extends Component {
+  
+  constructor(){
+    super();
+    this.state = {
+      route: 'page1',
+      component: ''
+    }
+  }
+
+  onRouteChange = (route) =>{
+    //No code splitting
+    //this.setState({route: route});
+
+    //code splitting thing:
+    if(route==='page1'){
+      this.setState({route: route});
+    } else if (route=== 'page2'){
+      import('./components/Page2')
+      .then( (Page2)=> {
+        this.setState({route: route, component: Page2.default})
+      }
+
+      );
+    } else if (route=== 'page3'){
+      import('./components/Page3')
+      .then( (Page3)=> {
+        this.setState({route: route, component: Page3.default})
+      }
+
+      );
+    }
+  }
+
+  render() {
+//    if (this.state.route === 'page1'){
+//      return <Page1 onRouteChange={this.onRouteChange} />
+//    } else if(this.state.route === 'page2'){
+//      return <Page2 onRouteChange={this.onRouteChange} />
+//    } else if(this.state.route === 'page3'){
+//      return <Page3 onRouteChange={this.onRouteChange} />
+//    }
+  
+    if (this.state.route === 'page1'){
+      return <Page1 onRouteChange={this.onRouteChange} />
+    } else {
+      return <this.state.component onRouteChange={this.onRouteChange} />
+    }
+
+  }
+}
+
+export default App;
